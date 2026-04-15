3d16075306b7a3bd40710a6e15f103eb3820b144
MrBT-nano <mr.bt1590@hotmail.com>
2026-04-15 22:36:17 +0700

    chore: scaffold project structure

---
+++ src/application/mod.rs
@@ -0,0 +1,1 @@
+// src/application/mod.rs
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod adapters;
+mod application;
+mod domain;
+mod ports;
+
+#[tokio::main]
+async fn main() {
+    println!("Chronos Foundation Starting...");
+}
